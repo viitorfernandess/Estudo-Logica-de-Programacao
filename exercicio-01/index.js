d7106d6e7a18f9985ae273002cbee6ea96d088a9
viitorfernandess <vitorazevedofer007@gmail.com>
2024-10-25 19:10:03 -0300

    exercício finalizado

---
+++ exercicio-01/index.js
@@ -0,0 +1,6 @@
+const numero = parseInt(prompt("Insira um número: "))
+
+const numeroAnterior = numero -1
+const numeroPosterior = numero +1
+
+alert("Número anterior é: " + numeroAnterior + "\nNúmero posterior é: " + numeroPosterior)
