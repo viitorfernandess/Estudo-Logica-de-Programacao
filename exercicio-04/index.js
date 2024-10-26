42d642db489ea2193732aa52ac67b0f5ab469868
viitorfernandess <vitorazevedofer007@gmail.com>
2024-10-26 12:41:27 -0300

    Cálculo de média de duas notas

---
+++ exercicio-04/index.js
@@ -0,0 +1,6 @@
+const nota1 = parseInt(prompt("Informe a primeira nota: "))
+const nota2 = parseInt(prompt("Informe a segunda nota: "))
+
+const mediaFinal = (nota1 + nota2) / 2
+
+alert("A média final é: " + mediaFinal)   
