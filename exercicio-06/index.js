4f024c221ca8bf1a69edabebaf0a723ca23d8e31
viitorfernandess <vitorazevedofer007@gmail.com>
2024-10-30 13:25:45 -0300

    Cálculo venda de veículo

---
+++ exercicio-06/index.js
@@ -0,0 +1,27 @@
+function mostrarPromocao () {
+// criar referências aos elemntos manipulados pelo programa
+var inVeiculo = document.getElementById("inVeiculo")
+var inPreco = document.getElementById("inPreco")
+var outVeiculo = document.getElementById("outVeiculo")
+var outEntrada = document.getElementById("outEntrada")
+var outParcela = document.getElementById("outParcela")
+
+// obtém o conteúdo dos campos de entrada
+var veiculo = inVeiculo.value
+var preco = inPreco.value
+
+// Calcular o valor da entrada e das parcelas
+var entrada = preco * 0.50
+var parcela = (preco * 0.50) / 12
+
+// altera o conteúdo dos parágrafos de rsposta
+outVeiculo.textContent = "Promoção: " + veiculo
+outEntrada.textContent = "Entrada de R$: " + entrada.toFixed(2)
+outParcela.textContent = "+ 12x de R$: " + parcela.toFixed(2)
+}
+
+// cria uma referência ao elemento btVerPromocao (botão)
+var btVerPromocao = document.getElementById("btVerPromocao")
+
+// registra um evento associado ao botão, para carregar uma função
+btVerPromocao.addEventListener(type="click", mostrarPromocao)
